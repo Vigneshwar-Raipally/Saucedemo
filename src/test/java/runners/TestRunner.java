be395bc778c64bc5af550cc07cfd6f4b7c97c40c
Jenkins CI <jenkins@pipeline.com>
2025-09-09 10:27:18 +0530

    Jenkins: Auto-commit after build

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -3,7 +3,7 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-// Cucumber
+// Making remote changes with Jenkins
 
 @CucumberOptions(
     features = "src/test/resources/features",
